b96f667c8b4a74cee8cf4eb780193f2644637f01
Pathompong Butpan <Pathompongmc@gmail.com>
2025-03-05 13:51:22 +0700

    Update API_TRT_Request.py

--- API_TRT_Request.py
+++ API_TRT_Request.py
@@ -1,40 +1,55 @@
-# Observation scheduling on TRT through an API request.
-
-# API address
 import requests
 import json
 
 url = "https://trt.narit.or.th/hub/api/newobservation"
-keep_id = []
 
-#Example for script observation to input parameters
-script= dict([("ObjectName", "M32"),                                                                                                 
-                ("StationName", "SRO"),                                                                                              
-                ("RA", "05:35:17.29"),                                                     
-                ("DEC", "-05:23:27.9"),                                                   
-                ("BinningXY", "1,1"),                                                                                                
-                ("CadenceInterval", "00:00:00"),                                                                                     
-                ("MaxAirmass", "3"),                                                                                                 
-                ("PA", "0"),                                                                                                         
-                ("Dither", "0"),                                                                                                     
-                ("ExpiryDate", "2021-07-31 16:11:21"),                                                                            
-                ("StartDate", "2021-06-20 16:11:21"),                                                                      
-                ("EndDate", "2021-06-26 16:11:21"),                                                                               
-                ("ExposuresMode", "1"),                                                                                              
-                ("M3Port", "1"),                                                                                                     
-                ("Filter", ["B", "V", "R", "I"]),                                                                                    
-                ("Suffix", ["{}_B".format(M32),"{}_V".format(M32),"{}_R".format(M32), "{}_I".format(M32)]),      
-                ("Exposure", ["120","120","120", "120"]),                                                                            
-                ("Repeat", ["5","5","5", "5"])                                                                                       
-              ])
-
-# Convert text to Json format
-payload = json.dumps({"script": [ script] })
-print(payload, "\n")
+payload = json.dumps({
+  "script": [
+    {
+      "ObjectName": "M53",
+      "StationName": "SRO",
+      "RA": "13:12:55.24",
+      "DEC": "+18:10:05.4",
+      "Subframe": "3",
+      "BinningXY": "1,1",
+      "CadenceInterval": "00:00:00",
+      "MaxAirmass": "3",
+      "PA": "0",
+      "Dither": "1",
+      "ExpiryDate": "2025-02-25 16:11:21",
+      "StartDate": "2025-01-30 16:11:21",
+      "EndDate": "2025-02-24 16:11:21",
+      "ExposuresMode": "1",
+      "M3Port": "1",
+      "Filter": [
+        "B",
+        "V",
+        "R"
+      ],
+      "Suffix": [
+        "suffixB",
+        "suffixV",
+        "suffixR"
+      ],
+      "Exposure": [
+        "6",
+        "6",
+        "6"
+      ],
+      "Repeat": [
+        "6",
+        "6",
+        "6"
+      ],
+      "isQuicklook": "1"
+    }
+  ]
+})
 headers = {
-              'Content-Type': 'application/json',
-              'TRT': 'your_token'
-          }
+  'Content-Type': 'application/json',
+  'TRT': 'yourtoken'
+}
+
 response = requests.request("POST", url, headers=headers, data=payload)
-keep_id.append(response.text)
+
 print(response.text)
